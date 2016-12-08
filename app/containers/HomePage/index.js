b737f9a3bf4793edbf733c6fe1678ad6982eb91e
Karlo Espiritu <karloespiritu@gmail.com>
2016-12-08 19:01:44 +0800

    add sample semantic components to homepage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,12 +13,16 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 import { Link } from 'react-router';
+import { Header, Button } from 'semantic-ui-react';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="row">
         <div className="col-md-4 col-md-offset-4">
+          <Header as="h1">Semantic UI Header Component</Header>
+          <Button primary>Semantic UI Primary Button</Button>
+          <Button secondary>Semantic UI Secondary Button</Button>
           <h1>
             <Link to="/login"><FormattedMessage {...messages.link} /></Link>
           </h1>
